ee53bc736a992afee5b0c1460b24d2c843eda6dd
konstantin.tskhovrebov <konstantin.tskhovrebov@jetbrains.com>
2022-01-21 13:31:07 +0100

    Update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 buildscript {
     repositories {
         gradlePluginPortal()
@@ -18,5 +20,20 @@ allprojects {
     }
 
     // ./gradlew dependencyUpdates
+    // Report: build/dependencyUpdates/report.txt
     apply(plugin = "com.github.ben-manes.versions")
+}
+
+//https://github.com/ben-manes/gradle-versions-plugin#rejectversionsif-and-componentselection
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
 }
